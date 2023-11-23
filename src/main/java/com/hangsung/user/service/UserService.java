adff6fcb8d1ce770e9e44c368495c7a770a85382
gundorit <packdev937@gmail.com>
2023-11-23 23:54:34 +0900

    feat : 회원 가입과 로그인 관련된 로직을 처리한다

---
+++ src/main/java/com/hangsung/user/service/UserService.java
@@ -0,0 +1,44 @@
+package com.hangsung.user.service;
+
+import com.hangsung.user.domain.User;
+import com.hangsung.user.domain.repository.UserRepository;
+import com.hangsung.user.request.SigninRequest;
+import com.hangsung.user.request.SignupRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final HttpSession session;
+
+    public void signup(SignupRequest signupRequest) {
+        if (userRepository.findByUsername(signupRequest.username()).isPresent()) {
+            throw new IllegalArgumentException("이미 존재 하는 아이디 입니다.");
+        }
+
+        User user = User.builder()
+            .username(signupRequest.username())
+            .password(signupRequest.password())
+            .nickname(signupRequest.nickname())
+            .birthAt(signupRequest.birthAt())
+            .gender(signupRequest.gender())
+            .build();
+
+        userRepository.save(user);
+    }
+
+    public void login(SigninRequest signinRequest) {
+        User user = userRepository.findByUsername(signinRequest.username())
+            .orElseThrow(() -> new IllegalArgumentException("존재 하지 않는 아이디 입니다."));
+
+        if (!signinRequest.password().equals(user.getPassword())) {
+            throw new IllegalArgumentException("아이디 또는 비밀번호가 맞지 않습니다. 다시 입력해주세요.");
+        }
+
+        session.setAttribute("user", user);
+    }
+}
