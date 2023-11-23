0aaa5da43985841c5fe7812d80f332e4aa0eed0b
gundorit <packdev937@gmail.com>
2023-11-23 11:24:22 +0900

    initial commit

---
+++ src/main/java/com/hangsung/HangsungApplication.java
@@ -0,0 +1,13 @@
+package com.hangsung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangsungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangsungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hangsung/HangsungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangsung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangsungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
